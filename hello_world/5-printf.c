ff6f3ebfdb6055ce949a390ce3d8bb68ecf66dc0
Klajdi2004 <klajdi.dhana1@gmail.com>
2023-02-16 10:33:08 -0800

    hello printf

---
+++ hello_world/5-printf.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ *  main - Entry point
+ *
+ *  Return always 0
+ *
+ *  <stdio> header
+ *
+ */
+
+int main()
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return 0;
+}
