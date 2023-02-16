e1548c598b59dc81d9f0fd6b0c178c4b53aa8423
Klajdi2004 <klajdi.dhana1@gmail.com>
2023-02-16 10:41:07 -0800

    hello printf

--- hello_world/5-printf.c
+++ hello_world/5-printf.c
@@ -1,5 +1,4 @@
 #include <stdio.h>
-
 /**
  *  main - Entry point
  *
@@ -7,10 +6,8 @@
  * 
  * a program to print  "with proper grammar, but the outcome is a piece of art"
  * 
- * 
  * Return: 0
  */
-
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
