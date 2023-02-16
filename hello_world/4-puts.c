bb27fefa8b81b9f2ba3e8ef2d4f99d474cb97c70
Klajdi2004 <klajdi.dhana1@gmail.com>
2023-02-16 10:19:02 -0800

    Hello, puts

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
- /*this is a betty commet */
-int main() 
-{ 
-	puts('"Programming is like building a multilingual puzzle\n');
-	return 0;
+
+int main() {
+	    puts("Programming is like building a multilingual puzzle");
+	        return 0;
 }
+
