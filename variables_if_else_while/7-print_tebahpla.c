4ef0e8ae425bc5677f3354239c3b06b3e64f47af
Klajdi2004 <klajdi.dhana1@gmail.com>
2023-02-20 13:34:15 -0800

    smile in the mirror

---
+++ variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main -  generates number 1 to 10
+ * Return: 0
+ */
+int main(void)
+{
+	char c;
+
+	for (c = 'z'; c >= 'a'; c--)
+	{
+		putchar(c);
+	}
+	putchar('\n');
+	return (0);
+}
