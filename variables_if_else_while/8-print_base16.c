f694b7386687886a085116e676b703101070bf75
Klajdi2004 <klajdi.dhana1@gmail.com>
2023-02-20 13:38:47 -0800

    hexadecimal

---
+++ variables_if_else_while/8-print_base16.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main -  generates number 1 to 10
+ * Return: 0
+ */
+int main(void)
+{
+	char c;
+
+	for (c = '0'; c <= '9'; c++)
+	{
+		putchar(c);
+	}
+	for (c = 'a'; c <= 'f'; c++)
+	{
+		putchar(c);
+	}
+	putchar('\n');
+	return (0);
+}
