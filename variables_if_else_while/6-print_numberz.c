972e984ab4c98bc364d005e978181971bf48f78f
Klajdi2004 <klajdi.dhana1@gmail.com>
2023-02-20 12:28:13 -0800

    numberz

---
+++ variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main -  generates number 1 to 10
+ * Return: 0
+ */
+int main(void)
+{
+	int dg;
+
+	for (dg = 0; dg < 10; dg++)
+	putchar(dg + '0');
+
+	putchar('\n');
+
+	return (0);
+}
