fb22b8ef3ee31eeed8028c62cdab202c4ddf7fb0
Klajdi2004 <klajdi.dhana1@gmail.com>
2023-02-21 01:51:16 -0800

    _putchar

---
+++ functions_nested_loops/0-putchar.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * main -  generates number 1 to 10
+ * Return: 0
+ */
+int main(void)
+{
+_putchar('p');
+_putchar('u');
+_putchar('t');
+_putchar('c');
+_putchar('h');
+_putchar('a');
+_putchar('r');
+_putchar('\n');
+return (0);
+}
