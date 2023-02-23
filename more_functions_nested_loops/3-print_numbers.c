1490a3f6c355121b4ef70b5835559184e6ee74ba
Klajdi2004 <klajdi.dhana1@gmail.com>
2023-02-23 13:04:15 -0800

    help

--- more_functions_nested_loops/3-print_numbers.c
+++ more_functions_nested_loops/3-print_numbers.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <unistd.h>
 
 /**
  * print_numbers -> print numbers
@@ -6,9 +7,9 @@
 
 void print_numbers(void)
 {
-	char c;
+	char i;
 
-	for (c = '0'; c <= '9'; c++)
-		_putchar(c);
+	for (i = 0; i < 10; i++)
+		_putchar(i);
 	_putchar('\n');
 }
