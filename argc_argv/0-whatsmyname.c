4ea979f74c53677b0b0a132e24026ae7c1ad1d54
Klajdi2004 <klajdi.dhana1@gmail.com>
2023-03-08 02:23:36 -0800

    prints its own name

---
+++ argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - main function
+ * @argc: -
+ * @argv: -
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+	argc = argc;
+	printf("%s\n", *argv);
+	return (0);
+}
