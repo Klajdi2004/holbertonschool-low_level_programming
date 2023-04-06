2bf427b2abeeba4fdbbb8051a3accbfa3d036e36
Klajdi2004 <klajdi.dhana1@gmail.com>
2023-04-06 02:13:55 -0700

    add node end

--- doubly_linked_lists/3-add_dnodeint_end.c
+++ doubly_linked_lists/3-add_dnodeint_end.c
@@ -1,40 +1,37 @@
 #include "lists.h"
 
 /**
- * add_dnodeint_end - Adds a new node at the end of a dlistint_t list.
- * @head: A pointer to a pointer to the head node of the list.
- * @n: The integer to store in the new node.
- *
- * Return: If memory allocation fails or head is NULL, return NULL.
- *         Otherwise, return the address of the new node.
- */
+ * add_dnodeint_end - adds a new node at the end of a dlistint_t list.
+ * @head: pointer to head pointer of the list
+ * @n: value of new node
+ * Return: address of new node or null if its fails
+ **/
 dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 {
-    dlistint_t *new, *temp;
+	dlistint_t *new, *tmp;
 
-    if (head == NULL)
-        return (NULL);
+	new = malloc(sizeof(dlistint_t));
+	if (new == NULL)
+	return (NULL);
+	new->n = n;
+	new->prev = NULL;
+	new->next = NULL;
 
-    new = malloc(sizeof(dlistint_t));
-    if (new == NULL)
-        return (NULL);
-
-    new->n = n;
-    new->next = NULL;
-
-    if (*head == NULL)
-    {
-        new->prev = NULL;
-        *head = new;
-        return (new);
-    }
-
-    temp = *head;
-    while (temp->next != NULL)
-        temp = temp->next;
-
-    temp->next = new;
-    new->prev = temp;
-
-    return (new);
+	if (*head == NULL)
+	{
+	*head = new;
+	return (new);
+	}
+	if ((*head)->next == NULL)
+	{
+	(*head)->next = new;
+	new->prev = *head;
+	return (new);
+	}
+	tmp = *head;
+	while (tmp->next)
+	tmp = tmp->next;
+	tmp->next = new;
+	new->prev = tmp;
+	return (new);
 }
