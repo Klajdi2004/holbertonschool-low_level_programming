ceb82b0d63c0c818e3e84e4f0c53abdde30f37cb
Klajdi2004 <klajdi.dhana1@gmail.com>
2023-04-14 01:58:11 -0700

    ok

--- hash_tables/3-hash_table_set.c
+++ hash_tables/3-hash_table_set.c
@@ -11,7 +11,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	unsigned long int index;
 	hash_node_t *new_node, *curr_node;
 
-	if (ht == NULL || key == NULL)
+	if (ht == NULL || key == NULL || value == NULL)
 		return (0);
 
 	index = key_index( (const unsigned char *)key, ht->size);
@@ -23,7 +23,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 		{
 			free(curr_node->value);
 			curr_node->value = strdup(value);
-			if (new_node->value == NULL)
+			if (curr_node->value == NULL)
 				return (0);
 			return (1);
 		}
@@ -33,8 +33,8 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	new_node = malloc(sizeof(hash_node_t));
 	if (new_node == NULL)
 		return (0);
-	new-node->key = strdup (key);
-	new_node->value - strdup (value);
+	new_node->key = strdup (key);
+	new_node->value = strdup (value);
 	if (new_node->value == NULL)
 		return (0);
 	new_node->next = ht->array[index];
